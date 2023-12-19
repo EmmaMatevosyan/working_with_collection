b0e3432c8da8d3a561bb9f86e804ba3ff52b6d7d
EmmaMatevosyan <131385066+EmmaMatevosyan@users.noreply.github.com>
2023-12-20 00:26:46 +0300

    Add files via upload

---
+++ working_with_collections/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"CsvReader"},{"p":"org.example","l":"Department"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Person"}];updateSearchResults();
